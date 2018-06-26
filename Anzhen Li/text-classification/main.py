616ec0b3d2cc50990841a3f61ab62ce54f6e2824
Arlenelalala <40139927+Arlenelalala@users.noreply.github.com>
2018-06-26 20:41:22 +0800

    Update main.py

--- Anzhen Li/text-classification/main.py
+++ Anzhen Li/text-classification/main.py
@@ -28,21 +28,21 @@ def train(path, rate):
 
 
 if __name__ == '__main__':
-    print('报告:要开始啦啦啦啦啦(/≧▽≦)/')
+    print('begin')
     t1 = time.time()
     path = "/Users/liangsong/Desktop/Corpus/0.txt"                             # 原文本所处的文件夹位置 @
     print('语料库位置: %s\n' % path)
 
-    print('✿✿ヽ(°▽°)ノ✿预处理✿✿ヽ(°▽°)ノ✿')
+    print('预处理')
     filepath_stop = 'ChineseStopWords.txt'                                      # 停用词位置 @
     pre_save_path = '/Users/liangsong/Desktop' + '/' + '预处理try'               # 预处理后，存放文本的位置 @
     filename = '预处理.txt'
     # pretreatment(filepath_stop, path, pre_save_path, filename)
     print('完成预处理操作\n')
 
-    print('✿✿ヽ(°▽°)ノ✿分类✿✿ヽ(°▽°)ノ✿')
+    print('分类')
     rate = 5                                                                      # 折叠数  @
     train(pre_save_path + '/' + filename, rate)
     t2 = time.time()
     print('花费时间：', t2-t1)
-    print('✿✿ヽ(°▽°)ノ✿end✿✿ヽ(°▽°)ノ✿')
+    print('end')
