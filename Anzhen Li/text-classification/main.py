5d8a7dc162a644e0fb68db38a89f9b8e78589e5c
Arlenelalala <40139927+Arlenelalala@users.noreply.github.com>
2018-06-26 19:59:03 +0800

    Add files via upload
    sklearn

---
+++ Anzhen Li/text-classification/main.py
@@ -0,0 +1,48 @@
+#coding: utf-8
+import matrix
+import pre
+import numpy as np
+import time
+
+np.set_printoptions(threshold=np.inf)
+
+
+def pretreatment(filepath_stop, path, pre_save_path, filename):
+    '''加载停用词'''
+    content = open(filepath_stop, 'r').read()
+    stopwords = content.splitlines()
+
+    ''' 文件操作，预处理操作 '''
+    # 处理文本
+    pretreat = pre.Pretreatment(path, stopwords)
+    pretreat.readfile()
+    # 保存
+    pretreat.stringlistsave(pre_save_path, filename)
+
+
+def train(path, rate):
+    train = matrix.Matrix()
+    train.divide(path, rate)
+    train.create_matrix()
+    train.train()
+
+
+if __name__ == '__main__':
+    print('报告:要开始啦啦啦啦啦(/≧▽≦)/')
+    t1 = time.time()
+    path = "/Users/liangsong/Desktop/Corpus/0.txt"                             # 原文本所处的文件夹位置 @
+    print('语料库位置: %s\n' % path)
+
+    print('✿✿ヽ(°▽°)ノ✿预处理✿✿ヽ(°▽°)ノ✿')
+    filepath_stop = 'ChineseStopWords.txt'                                      # 停用词位置 @
+    pre_save_path = '/Users/liangsong/Desktop' + '/' + '预处理try'               # 预处理后，存放文本的位置 @
+    filename = '预处理.txt'
+    # pretreatment(filepath_stop, path, pre_save_path, filename)
+    print('完成预处理操作\n')
+
+    print('✿✿ヽ(°▽°)ノ✿分类✿✿ヽ(°▽°)ノ✿')
+    rate = 5                                                                      # 折叠数  @
+    train(pre_save_path + '/' + filename, rate)
+    t2 = time.time()
+    print('花费时间：', t2-t1)
+    print('✿✿ヽ(°▽°)ノ✿end✿✿ヽ(°▽°)ノ✿')
---
+++ Anzhen Li/text-classification/pre.py
@@ -0,0 +1,81 @@
+# -*-coding:utf-8-*-
+import re
+import thulac
+from pyhanlp import *
+import multiprocessing
+
+'''对文件进行预处理操作'''
+'''Pretreatment类是用于对单个txt文件，读取、预处理、存储'''
+'''调用时要进行文件夹批量操作'''
+
+
+class Pretreatment(object):
+    def __init__(self, path, stopwords):
+        self.filepath = path
+        self.text = []
+        self.text_cut_all = {}
+        # self.thu1 = thulac.thulac(T2S=True, seg_only=True, filt=True)
+        self.stopwords = stopwords
+        self.textlist = {}  # 记录每类下所有的文章
+
+
+    '''读文件内容，同时进行预处理'''
+    def readfile(self):
+        # pool = multiprocessing.Pool()
+        with open(self.filepath, 'r') as file:
+            self.text = file.read().split('820660C500')
+            for text_list in self.text[1:]:
+                text_list_filter = []
+                text_list2 = text_list.strip().split('txttxttxttxt\t')
+                i = 0
+                num = text_list2[0].strip('\n')
+                for text_line in text_list2[1:]:
+                    i += 1
+                    text = self.filter(text_line)
+                    text_list_filter.append(text)
+                    # pool.apply_async(self.mult_filter, (text_line,))
+                # pool.close()
+                # pool.join()
+                self.textlist[num] = text_list_filter
+                print('预处理完成的是%s类，共%d项' % (num, i))
+
+
+    '''一个文本的预处理（分词、过滤）'''
+    def filter(self, text_line):
+        text_cut_one = []
+        # text_cuts = self.thu1.cut(text_line, text=True)            # @ 清华切词
+        # text_cuts = text_cuts.split(' ')                           # @
+        text_cuts = HanLP.segment(text_line)                        # @@ HanLp切词
+        for text_cut in text_cuts:
+            text_cut = re.findall(r'(.*?)/', str(text_cut))[0]    # @@
+            if text_cut not in self.stopwords:
+                text_cut = re.sub(u'[a-zA-Z0-9’!"#$%&\'()*+,-./:;<=>?@，。?★、…【】《》？“”‘’！[\\]^_`{|}~]+',
+                                  '', text_cut, re.S and re.M)
+                if text_cut != '\t' and '\n':
+                    text_cut_one.append(text_cut)
+        text = ' '.join(text_cut_one)
+        return text
+
+
+    '''存文本'''
+    def stringlistsave(self, save_path, filename):
+        path = save_path + "/" + filename
+        try:
+            os.remove(path)
+        except FileNotFoundError:
+            pass
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+
+        with open(path, "a+") as fp:
+            for key in self.textlist:
+                fp.write('820660C500' + key + '\n')
+                for text in self.textlist[key]:
+                    fp.write("txttxttxttxt\t%s " % text)
+        print('预处理存储OK，存储位置：%s' % path)
+
+
+
+
+
+
