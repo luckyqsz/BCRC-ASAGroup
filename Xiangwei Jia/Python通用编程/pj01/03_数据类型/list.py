c966e3bc5974ce4bedb04ec12d0ba6c1c5195ff9
Gavin <38515498+Xw-Jia@users.noreply.github.com>
2019-01-22 00:00:49 +0800

    Add files via upload

---
+++ Xiangwei Jia/Python通用编程/pj01/03_数据类型/list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python 
+#!-*-coding:utf-8 -*-
+#!@Time		: 2019/1/21 0021	23:05
+#!@Author	: XW_Jia
+#!@File		: list.py
+l1 = ['a', 'b', 'c', 'd', 'e', 'b', 'c']
+l1.clear()
+print(l1)
